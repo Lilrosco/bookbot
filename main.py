de30ab35234703a6da05b476de9462b8ed8dd310
Lilrosco <rossey.charleston@gmail.com>
2025-05-20 23:15:51 -0400

    Can now give a report to any book location provided.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import *
+import sys
 
 def get_book_text(path_to_file):
     file_contents = None
@@ -8,23 +9,30 @@ def get_book_text(path_to_file):
     
     return file_contents
 
-def main():
-    book_location = "./books/frankenstein.txt"
-    book_text = get_book_text(book_location)
-
+def print_report(book_location, num_of_words, sorted_char_list):
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_location}")
     print("----------- Word Count ----------")
-    num_of_words = count_words(book_text)
     print(f"Found {num_of_words} total words")
     print("--------- Character Count -------")
     print(
         pretty_print_character_stats(
-            sorted_character_stat(
-                count_characters(book_text)
-            )
+            sorted_char_list
         )
     )
     print("============= END ===============")
+
+def main():
+    if len(sys.argv) < 2:
+        print("Missing arguments")
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_location = sys.argv[1]
+    book_text = get_book_text(book_location)
+    num_of_words = count_words(book_text)
+    sorted_char_list = sorted_character_stat(count_characters(book_text))
+
+    print_report(book_location, num_of_words, sorted_char_list)
 
 main()
